a77c2dfdf08e83ad969538bc26efa9e818a0ad7f
SadoALikhan <khan.ali.99.sado@mail.ru>
2024-05-05 14:29:29 +0300

    Homework

---
+++ src/ServiceStation.java
@@ -0,0 +1,7 @@
+public interface ServiceStation {
+    void check(Bicycle[] bicycles);
+
+    void check(Car[] cars);
+
+    void check(Truck[] trucks);
+}
---
+++ src/ServiceStationForTransport.java
@@ -0,0 +1,6 @@
+public class ServiceStationForTransport implements ServiceStation {
+    @Override
+    public void check(Bicycle[] bicycles) {
+        System.out.println("Обслуживаем " + bicycles.g);
+    }
+}
